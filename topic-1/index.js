388a282ab5f42a6d7c41455b39be08f81c6e54a7
blackjac7 <febriansyah705@gmail.com>
2024-04-19 21:22:13 +0700

    test deploy 5

--- topic-1/index.js
+++ topic-1/index.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const fs = require("fs");
-const path = "/workspace";
+const path = require("path");
+const dir = path.join(__dirname, "/workspace");
 const fileUpload = require("express-fileupload");
 const routes = require("./router");
 const errorHandlers = require("./middleware/errorHandlers");
@@ -10,8 +11,8 @@ const errorHandlers = require("./middleware/errorHandlers");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-if (!fs.existsSync(path)) {
-    fs.mkdirSync(path, { recursive: true });
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
 }
 
 app.use(express.json());
