13389ac355dc11847b0635899ad2afeb4128d15f
blackjac7 <febriansyah705@gmail.com>
2024-04-19 21:45:09 +0700

    test deploy 7

--- topic-1/index.js
+++ topic-1/index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const dir = path.join(__dirname, "tmp");
 const fileUpload = require("express-fileupload");
 const routes = require("./router");
 const errorHandlers = require("./middleware/errorHandlers");
@@ -11,12 +8,13 @@ const errorHandlers = require("./middleware/errorHandlers");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-}
-
 app.use(express.json());
-app.use(fileUpload({ useTempFiles: true }));
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: process.env.NODE_ENV == "development" ? "./tmp" : "/tmp", // if you're using GCP App Engine please don't comment this, because the ./tmp directory is read only and we need write too so we use /tmp
+    })
+);
 
 app.use(express.static("public"));
 
